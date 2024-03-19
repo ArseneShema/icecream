72a059a276eb5e7d3c22e30fd8aecd92e3f9d7ee
Arsene Shema <arsenejb21@gmail.com>
2024-03-19 16:08:42 -0600

    icecream

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("ordering vanilla")
+
+let items =
+
+    [
+        {
+            name: 'Cookie Dough',
+            price: 1.25,
+            quantity: 0
+        },
+        {
+            name: 'Vanilla',
+            price: 1,
+            quantity: 0
+        },
+        {
+            name: 'Strawberry',
+            price: 1.25,
+            quantity: 0
+        }],
+
+    Toppings = [{
+        name: 'Sprinkles',
+        quantity: 0,
+        price: .25
+    },
+    {
+        name: 'Chocolate Chips',
+        price: .25,
+        quantity: 0
+    },
+    {
+        name: 'Cookie Chunks',
+        price: .5,
+        quantity: 0
+    }]
